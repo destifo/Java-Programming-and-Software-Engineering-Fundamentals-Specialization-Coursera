b8a5d911b100e2397279634dd294ed444e33ed3e
destifo <estifanosbireda@gmail.com>
2021-11-25 12:54:14 +0300

    added the make grey feature

--- course_one/upload_files/upload.js
+++ course_one/upload_files/upload.js
@@ -1,6 +1,22 @@
+var img;
+var canvas;
+var greyImg;
+
 function upload(file){
-    var canvas = document.getElementById("cnv1");
+    canvas = document.getElementById("cnv1");
     var filename = document.getElementById("finput");
-    var img = new SimpleImage(filename);
+    greyImg = new SimpleImage(filename);
+    img = new SimpleImage(filename);
     img.drawTo(canvas);
+}
+
+function makeGrey(){
+    for (var pixel of greyImg.values()){
+        var avg = (pixel.getRed() + pixel.getBlue() + pixel.getGreen())/3;
+        pixel.setRed(avg);
+        pixel.setBlue(avg);
+        pixel.setGreen(avg);
+    }
+    var greyCanvas = document.getElementById("cnv2");
+    greyImg.drawTo(greyCanvas);
 }
