fcb17e70ede17839fe650099015d32dfc8afe337
destifo <estifanosbireda@gmail.com>
2021-11-25 15:05:19 +0300

    5 star bugs bunny: dont try it at home or school

---
+++ course_one/GreenScreenWebPage/greenScreen.js
@@ -0,0 +1,50 @@
+var fimg = null;
+var bimg = null;
+var fcanvas = null;
+var bcanvas = null;
+var oimg = null;
+
+function loadFgImg(file){
+    fcanvas = document.getElementById("cnv1");
+    fimg = new SimpleImage(file);
+    fimg.drawTo(fcanvas);
+    
+}
+
+function loadBgImg(file){
+    bcanvas = document.getElementById("cnv2");
+    bimg = new SimpleImage(file);
+    bimg.drawTo(bcanvas);
+}
+
+function clrCanvas(){
+    var fCtx = fcanvas.getContext("2d");
+    var bCtx = bcanvas.getContext("2d");
+    fCtx.clearRect(0, 0, fcanvas.width, fcanvas.height);
+    bCtx.clearRect(0, 0, bcanvas.width, bcanvas.height);
+}
+
+function doGreenScreen(){
+    if (bimg == null || !bimg.complete()){
+        alert('Error while accessing the background image');
+        return;
+    }
+    if (fimg == null || !bimg.complete()){
+        alert('Error while accessing the foreground image');
+        return;
+    }
+
+    for (var pixel of fimg.values()){
+        var x = pixel.getX();
+        var y = pixel.getY();
+        if (pixel.getGreen() > (pixel.getBlue() + pixel.getRed())){
+            oimg = new SimpleImage(fimg.width, fimg.height);
+            oimg.setPixel(x, y, bimg.getPixel(x, y));
+        }
+        else{
+            oimg.setPixel(x, y, pixel);
+        }
+    }
+    clrCanvas();
+    oimg.drawTo(fcanvas);
+}
